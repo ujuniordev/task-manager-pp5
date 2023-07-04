57bf6dabbcbb47fa4b1ef9459575b1a099ab7d38
ujuniordev <ujunior@outlook.com>
2023-07-04 20:53:31 +0000

    Add loader using Context API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
+import { Spinner } from "reactstrap";
+import TodoList from "./pages/TodoList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Context from "./Context";
+import { useState } from "react";
 import "./App.css";
 import "./api/axiosDefaults";
-import TodoList from "./pages/TodoList";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [appState, setAppState] = useState({ loading: false });
   return (
-    <div className="App">
-      <h1 className="text-uppercase text-center my-4">Todo app</h1>
+    <div className="task-manager">
+      <h1 className="text-uppercase text-center my-4">Task Manager</h1>
       <div className="row">
         <div className="col-md-6 col-sm-10 mx-auto p-0">
           <div className="card p-3">
-            <Router>
-              <Switch>
-                <Route path="/signin">
-                  <div>SignIn Component</div>
-                </Route>
-                <Route path="/signup">
-                  <div>SignOut Component</div>
-                </Route>
-                <Route path="/">
-                  <TodoList />
-                </Route>
-              </Switch>
-            </Router>
+            {appState.loading && (
+              <div className="loader">
+                <Spinner color="primary" />
+                <span>Loading...</span>
+              </div>
+            )}
+            <Context.Provider value={[appState, setAppState]}>
+              <Router>
+                <Switch>
+                  <Route path="/signin">
+                    <div>SignIn Component</div>
+                  </Route>
+                  <Route path="/signup">
+                    <div>SignOut Component</div>
+                  </Route>
+                  <Route path="/">
+                    <TodoList />
+                  </Route>
+                </Switch>
+              </Router>
+            </Context.Provider>
           </div>
         </div>
       </div>
