de73a0c2d40ccd354debda261e7495b28ab7466c
ujuniordev <ujunior@outlook.com>
2023-07-05 09:22:11 +0000

    Add interceptor to axiosInstance.js to handle access errors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -6,3 +6,22 @@ axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+customAxios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      switch (error.response.status) {
+        case 401 || 403:
+          localStorage.clear("tmng-user");
+          window.location = "/signin";
+          break;
+  
+        default:
+          return Promise.reject(error);
+      }
+    }
+  );
+  
+  export default customAxios;
