07080479b6a7eb6e277db29f6ccd364bae2f9a5d
ujuniordev <ujunior@outlook.com>
2023-07-05 15:00:03 +0000

    Remove custom Axios dev if

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,32 +3,26 @@ import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 const customAxios = axios.create({
-  baseURL:
-    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-      ? "/"
-      : "https://drf-task-manager.herokuapp.com/",
-  headers: {
-    "X-Custom-Header": "foobar",
-  },
+  baseURL: "https://drf-task-manager.herokuapp.com/",
 });
 
 customAxios.withCredentials = true;
 
 customAxios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      switch (error.response.status) {
-        case 401 || 403:
-          localStorage.clear("tmng-user");
-          window.location = "/signin";
-          break;
-  
-        default:
-          return Promise.reject(error);
-      }
+  (response) => {
+    return response;
+  },
+  (error) => {
+    switch (error.response.status) {
+      case 401 || 403:
+        localStorage.clear("tmng-user");
+        window.location = "/signin";
+        break;
+
+      default:
+        return Promise.reject(error);
     }
-  );
-  
-  export default customAxios;
+  }
+);
+
+export default customAxios;
